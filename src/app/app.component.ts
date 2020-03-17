d96bdd2f5324b1f5b04eafdec68873a2b5aa398f
achrafHaddad <achrafmail7@gmail.com>
2020-03-17 14:48:03 +0100

    skills problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {CdkTextareaAutosize} from '@angular/cdk/text-field';
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
 import {MatChipInputEvent} from '@angular/material/chips';
@@ -12,58 +12,57 @@ import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cv';
 
-  newExp = new FormGroup ({
-    
-    post: new FormControl('', [Validators.required]),
-    societe: new FormControl('', [Validators.required]),
-    dateDebut: new FormControl('', [Validators.required]),
-    dateFin: new FormControl('', [Validators.required])
-  })
-
-
-  angFormInfo = new FormGroup({
-    presonalInfo: new FormGroup({
-
-      nom: new FormControl('', [Validators.required, Validators.minLength(4), Validators.pattern("^[a-zA-Z]+$")]),
-      prenom: new FormControl('', [Validators.required, Validators.minLength(4), Validators.pattern("^[a-zA-Z]+$")]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      tel: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(8),
-        Validators.pattern("^[0-9]$")]),
-      adresse: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    }),
-    exps: new FormArray([this.newExp]),
-    skills: new FormArray([])  
-  });
-
-  
-  
-
+  angFormInfo: FormGroup;
 
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
   
-
-  get exps(): FormArray {
+  get allExps(): FormArray {
     return this.angFormInfo.get('exps') as FormArray;
   }
   onFormSubmit(): void {
     
       console.log(this.angFormInfo.value);
+  }
+  newExp(): FormGroup {
+   return  new FormGroup ({
     
+      post: new FormControl('', [Validators.required]),
+      societe: new FormControl('', [Validators.required]),
+      dateDebut: new FormControl('', [Validators.required]),
+      dateFin: new FormControl('', [Validators.required])// zidni condition fel html mouch lenna elli date fin dima akber mel date debut
+    })
   }
   addNameField() {
-    this.exps.push(this.newExp);
+    this.allExps.push(this.newExp());
   }
 
   deleteNameField(index: number) {
-    if (this.exps.length !== 1) {
-      this.exps.removeAt(index);
+    if (this.allExps.length !== 1) {
+      this.allExps.removeAt(index);
     }
-    console.log(this.exps.length);
+    console.log(this.allExps.length);
   }
+
+  ngOnInit(): void {
+    this.angFormInfo = new FormGroup({
+      presonalInfo: new FormGroup({
+  
+        nom: new FormControl('', [Validators.required, Validators.minLength(4), Validators.pattern("^[a-zA-Z]+$")]),
+        prenom: new FormControl('', [Validators.required, Validators.minLength(4), Validators.pattern("^[a-zA-Z]+$")]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        tel: new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(8),
+          Validators.pattern("^[0-9]$")]),
+        adresse: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      }),
+      exps: new FormArray([this.newExp()]),
+      skills: new FormArray([])  
+    });
+  }
+
 
   visible = true;
   selectable = true;
@@ -71,7 +70,7 @@ export class AppComponent {
   separatorKeysCodes: number[] = [ENTER, COMMA];
   skillCtrl = new FormControl();
   filteredskills: Observable<string[]>;
-  skills: string[] = ['HTML'];
+  newSkill: string[] = [];
   allskills: string[] = ['HTML', 'CSS', 'Javascript', 'Angular', 'MangoDB', 'Java'];
 
   @ViewChild('skillInput') skillInput: ElementRef<HTMLInputElement>;
@@ -83,13 +82,20 @@ export class AppComponent {
         map((skill: string | null) => skill ? this._filter(skill) : this.allskills.slice()));
   }
 
+  
+
+  // get skills(){
+  //   return this.angFormInfo.get('skills') as FormArray
+  // }
+  
+
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
     // Add our skill
     if ((value || '').trim()) {
-      this.skills.push(value.trim());
+      this.newSkill.push(value.trim());
     }
 
     // Reset the input value
@@ -101,15 +107,15 @@ export class AppComponent {
   }
 
   remove(skill: string): void {
-    const index = this.skills.indexOf(skill);
+    const index = this.newSkill.indexOf(skill);
 
     if (index >= 0) {
-      this.skills.splice(index, 1);
+      this.newSkill.splice(index, 1);
     }
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    this.skills.push(event.option.viewValue);
+    this.newSkill.push(event.option.viewValue);
     this.skillInput.nativeElement.value = '';
     this.skillCtrl.setValue(null);
   }
